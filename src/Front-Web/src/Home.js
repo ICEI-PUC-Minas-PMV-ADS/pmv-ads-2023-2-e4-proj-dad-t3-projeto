1d6c24c719950832a2915b575c79ae4e62221a61
Alex Berton <alexberton47@gmail.com>
2023-10-21 12:08:34 -0300

    Padronização de telas.
    Apenas alterei as telas pada o padrão de informação e formatação. Só não alterei a tela de "Faturamento".

--- src/Front-Web/src/Home.js
+++ src/Front-Web/src/Home.js
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
 import React from 'react';
 import Sidebar from './Sidebar';
 import './Style.css';
+import SeletorData from './components/SeletorData';
 
 function Home() {
+  const date = new Date();
+  const [data, setData] = useState([date.getMonth(), date.getFullYear()]);
   return (
-    <div className= "home">
+    <div className="main-all">
       <Sidebar />
-      <h1>Home Page</h1>
-      {/* Adicione o conteúdo da página Home aqui */}
+      
+      <main>
+        <div className="main-header">
+          <h1 className="main-title">Dashborad</h1>
+          <h2 className="main-subtitle">Acompanhe suas finanças</h2>
+        </div>
+
+        <div className="main-container">
+          <div className="page-header">
+            <h1 className="main-title2"></h1>
+            <SeletorData getDate={setData} />
+          </div>
+          
+        </div>
+      </main>
     </div>
   );
 }
--- src/Front-Web/src/Suporte.js
+++ src/Front-Web/src/Suporte.js
@@ -4,10 +4,22 @@ import './Style.css';
 
 function Suporte() {
   return (
-    <div className="suporte">
+    <div className="main-all">
       <Sidebar />
-      <h1>Suporte</h1>
-      {/* Adicione o conteúdo da página Suporte aqui */}
+      
+      <main>
+        <div className="main-header">
+          <h1 className="main-title">Suporte</h1>
+          <h2 className="main-subtitle">Contate-nos</h2>
+        </div>
+
+        <div className="main-container">
+          <div className="page-header">
+            <h1 className="main-title2">projeto@gerenciar.com.br</h1>
+          </div>
+          
+        </div>
+      </main>
     </div>
   );
 }
