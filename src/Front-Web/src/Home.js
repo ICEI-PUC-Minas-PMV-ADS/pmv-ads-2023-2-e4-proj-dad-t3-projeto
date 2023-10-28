19eb1238a038ac7f0e2a5f9ed619d53ee30f34f2
erickreis <erick.reis2@gmail.com>
2023-10-28 16:12:05 -0300

    Graficos tela home

--- src/Front-Web/src/Home.js
+++ src/Front-Web/src/Home.js
@@ -1,18 +1,38 @@
-import { useState } from 'react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from './Sidebar';
 import './Style.css';
 import SeletorData from './components/SeletorData';
-
+import FaturamentoChart from './components/FaturamentoChart';
+import useAxios from './hooks/useAxios';
 
 function Home() {
   const date = new Date();
   const [data, setData] = useState([date.getFullYear(), date.getMonth() + 1]); // Altere o valor padrão conforme necessário
+  const token = localStorage.getItem('token');
+  const [valorFaturado, setValorFaturado] = useState(null);
+  const [valorVendas, setValorVendas] = useState(null);
+  const { response, loading, error } = useAxios({
+    method: 'get',
+    url: `Faturamento/data?Ano=${data[0]}&Mes=${data[1]}`,
+    headers: JSON.stringify({
+      Authorization: 'Bearer ' + token,
+    }),
+  });
+
+  useEffect(() => {
+    if (response && !error) {
+      setValorFaturado(response.map((item) => item.valorFaturadoMes));
+      setValorVendas(response.map((item) => item.numeroVendas));
+    } else if (error) {
+      console.log(error);
+      setValorFaturado(null);
+    }
+  }, [data, response, error]);
 
   return (
     <div className="main-all">
       <Sidebar />
-      
+
       <main>
         <div className="main-header">
           <h1 className="main-title">Dashboard</h1>
@@ -21,11 +41,33 @@ function Home() {
 
         <div className="main-container">
           <div className="page-header">
-            <h1 className="main-title2"></h1>
+            <h1 className="main-title2"> </h1>
             <SeletorData getDate={setData} />
           </div>
-          
 
+          {/* Adicione o componente FaturamentoChart aqui e passe 'ano' e 'mes' como props */}
+          <FaturamentoChart
+            ano={data[0]}
+            mes={data[1]}
+            values={valorFaturado}
+            title={'Valor Faturado Diário'}
+            label={'Valor Vendas'}
+            type={'line'}
+            colorBackGround={'rgba(210, 55, 110,0.4)'}
+            colorBorder={'rgba(220, 55, 110,0.8)'}
+            titleCircle={'Total Valor Faturado'}
+          />
+          <FaturamentoChart
+            ano={data[0]}
+            mes={data[1]}
+            values={valorVendas}
+            title={'Número de Venas Diária'}
+            label={'Número Vendas'}
+            type={'bar'}
+            colorBackGround={'rgba(10, 55, 110,0.4)'}
+            colorBorder={'rgba(10, 55, 110,0.8)'}
+            titleCircle={'Total Itens Vendidos'}
+          />
         </div>
       </main>
     </div>
---
+++ src/Front-Web/src/components/FaturamentoChart.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+} from 'chart.js';
+import { Line, Bar } from 'react-chartjs-2';
+import 'chartjs-plugin-annotation';
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const FaturamentoChart = ({
+  ano,
+  mes,
+  values,
+  title,
+  label,
+  type,
+  colorBackGround,
+  colorBorder,
+  titleCircle,
+}) => {
+  const [dias, setDias] = useState([]);
+  const [totalValue, setTotalValue] = useState(0);
+  let charValues = values;
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: title,
+      },
+    },
+  };
+
+  useEffect(() => {
+    try {
+      if (values.length === 0) charValues = [];
+      setTotalValue(values.reduce((a, b) => a + b, 0));
+      const diasLength = [];
+      for (let i = 0; i < values.length; i++) {
+        diasLength.push(i + 1);
+      }
+      setDias(diasLength);
+      console.log(chartData);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [ano, mes, values]);
+
+  const chartData = {
+    labels: dias.map((item) => `Dia ${item}`),
+    datasets: [
+      {
+        label: label,
+        data: values,
+        backgroundColor: colorBackGround,
+        borderColor: colorBorder,
+        borderWidth: 5,
+        borderRadius: 10,
+        maxBarThickness: 80,
+        hoverBackgroundColor: colorBorder,
+      },
+    ],
+  };
+
+  return (
+    <div className="chart-container">
+      <div className="chart-item">
+        {type === 'line' ? (
+          <Line options={options} data={chartData} width={200} height={60} />
+        ) : (
+          <Bar options={options} data={chartData} width={200} height={60} />
+        )}
+      </div>
+      <div className="circle-container">
+        <h3>{titleCircle}</h3>
+        <div className="circle" style={{ border: `14px solid ${colorBorder}` }}>
+          <h1>{totalValue}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaturamentoChart;
